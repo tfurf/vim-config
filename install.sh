54eb4e3340d43ecd07a9d97c4dcbf24ef72cc1ab
Thomas Furfaro <furfaro@cmre.nato.int>
2014-04-17 21:51:57 +0200

    More cleanups.

--- install.sh
+++ install.sh
@@ -3,9 +3,20 @@
 cwd=$(dirname $0); 
 t=".vimrc"
 
+while getopts f? k; do
+  case "${k}" in
+    f)
+      force="yes"
+      ;;
+    ?)
+      exit 1
+      ;;
+  esac
+done
+
 for f in ${t}; do
   [[ -f ${cwd}/${f} ]] && { 
+    [[ ${force} == "yes" ]] && mv -v ~/${f} ~/${f}.bak;
     ln -vT ${cwd}/${f} ~/${f} ;
-    echo ${f};
   }
 done;
